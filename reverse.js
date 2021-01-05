f3e9914ebda001707d951df699c6e02ae56edad9
veda1612 <vedavasavisiri@gmail.com>
2021-01-05 17:57:46 -0500

    Add files via upload

---
+++ reverse.js
@@ -0,0 +1,9 @@
+var input = process.argv[2];
+
+if (input)
+  console.log(reverse(input));
+
+function reverse(original) {
+  //return original.split('').reverseList().join('');
+  return original.split('').reverse().join('');
+}
